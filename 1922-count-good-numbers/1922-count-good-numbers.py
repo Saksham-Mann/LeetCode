885625a85f302b1b559cd738d2ef83c8fbeff0ec
Saksham Mann <sakshammann4@gmail.com>
2026-09-11 20:47:48 +0530

    Time: 3 ms (20.64%), Space: 19.4 MB (23.53%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,11 +1,6 @@
 class Solution:
-    def p(self, x: int, n: int) -> int:
-        if x==1 or n==0:
-            return 1
-        if n&1==0:
-            return self.p((x*x)%1000000007,n//2)
-        return (x*(self.p(x,n-1)))%(1000000007)
     def countGoodNumbers(self, n: int) -> int:
+        m=1000000007
         prime=n//2
         even=(n+1)//2
-        return (self.p(5,even)*self.p(4,prime))%(1000000007)
+        return (pow(5,even,m)*pow(4,prime,m))%m
